0538363ca638d37c4cd54d0c946fa49779d71583
Thiago Camargo <thiagocamargo@e.email>
2021-02-04 11:45:40 -0300

    Allow message exchange between client and server

--- client.py
+++ client.py
@@ -1,5 +1,21 @@
 import socket
 
+# Create socket
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Try to connect
 client_socket.connect(("127.0.0.1", 8081))
+
 print("Client has connected to the server")
+
+# Getting data from server using the socket
+
+data = client_socket.recv(1024)
+
+message = data.decode()
+
+print(message)
+
+# Time for some closure
+
+client_socket.close()
--- server.py
+++ server.py
@@ -1,8 +1,30 @@
 import socket
 
+# Create a socket
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Bind the socket
 server_socket.bind = (("0.0.0.0", 8081))
+
+# Enable listening
 server_socket.listen()
+
 print("Server is listening...")
+
+# Accept connection
 connection_socket, address = server_socket.accept()
+
 print("Client connected")
+
+# Send something to the client
+
+message = "Hey there, thank you for connecting with me"
+
+data = message.encode()
+
+connection_socket.send(data)
+
+# Time for some closure
+
+connection_socket.close()
+server_socket.close()
