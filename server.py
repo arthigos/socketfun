237841a4ee1c3664439cf4710b89edeb6f965b1d
Thiago Camargo <thiagocamargo@e.email>
2021-02-04 11:55:23 -0300

    Fix AttributeError: 'socket' object attribute 'bind' is read-only

--- server.py
+++ server.py
@@ -4,17 +4,17 @@ import socket
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Bind the socket
-server_socket.bind = (("0.0.0.0", 8081))
+server_socket.bind(("0.0.0.0", 8081))
 
 # Enable listening
 server_socket.listen()
 
-print("Server is listening...")
+print("Server is listening at port 8081")
 
 # Accept connection
 connection_socket, address = server_socket.accept()
 
-print("Client connected")
+print("Connected with ", client_address)
 
 # Send something to the client
 
@@ -23,6 +23,13 @@ message = "Hey there, thank you for connecting with me"
 data = message.encode()
 
 connection_socket.send(data)
+
+# Hearing back from the client
+client_message = connection_socket.recv(1024)
+
+client_message.decode()
+
+print(client_message)
 
 # Time for some closure
 
